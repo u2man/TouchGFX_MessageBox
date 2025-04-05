aa73b4548f50ca3030cec281958bfe38aa760cec
Kang Usman <u_2man@yahoo.co.id>
2025-04-05 21:07:47 +0700

    Add files via upload

---
+++ MessageBoxExample/config/gcc/app.mk
@@ -0,0 +1,8 @@
+
+# Relative location of the TouchGFX framework from root of application
+touchgfx_path := touchgfx
+
+# Location of the TouchGFX Environment
+touchgfx_env := D:/TouchGFX/4.25.0/env
+# Optional additional compiler flags
+user_cflags := -DUSE_BPP=24
